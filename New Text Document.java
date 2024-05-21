105409096a3961076ad0e231e1101be0ff0cd031
Shankar Kumar <shankark@sti.com>
2024-05-21 16:12:25 +0530

    this is shankar code

---
+++ New Text Document.java
@@ -0,0 +1,26 @@
+package Practics_programe;
+
+public class miisingnumber {
+
+	public static void main(String[] args) {
+
+
+		int arr[]= {1,2,3,4,7,8,9,10};
+		int j=0;
+		for(int i=1;i<arr.length;i++)
+		{
+
+			if(i!=arr[j])
+			{
+
+				System.out.println(i);
+
+			}else {
+				j++;
+
+			}
+		}
+
+	}
+
+}
